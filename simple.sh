965e35c63274bc1758cc6dd781063ea8f00ea652
Wink Saville <wink@saville.com>
2019-05-04 16:19:28 -0700

    Update simple.sh
     - Add compiling lld
     - Add set -o pipefail so exit status is from `${cmd}`

--- simple.sh
+++ simple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Builds llvm;clang;compiler-rt in src/build-${id} with
+# Builds llvm;clang;lld;compiler-rt in src/build-${id} with
 # logs in src/log-${id].txt and install in ~/local-${id}
 
 id=$1
@@ -18,13 +18,17 @@ if [[ ${jobcnt} < 1 ]]; then jobcnt=1; fi
 rm -f ${log}
 touch ${log}
 
-# Use set -x so we see the commands
+# Use set -x so we see the commands.
+# Substitue check-all for others like check-tsan:
 cmd="set -x ; \
-  cmake ../llvm -G Ninja -DLLVM_ENABLE_PROJECTS=\"clang;compiler-rt\" -DCMAKE_INSTALL_PREFIX=${install_dir} -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_LINKER=gold && \
+  cmake ../llvm -G Ninja -DLLVM_ENABLE_PROJECTS=\"clang;lld;compiler-rt\" -DCMAKE_INSTALL_PREFIX=${install_dir} -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_LINKER=gold && \
   ninja -j${jobcnt} -v && \
   ninja -j${jobcnt} -v check-all"
 
-# In a subshell time the evaluated command and then log the output
+# Set the pipefail flag so the exit status is from ${cmd} and not time or tee
+set -o pipefail
+
+# In a subshell time the evaluated command and log the output
 # of the subshell with 2>&1 | tee $(log). The log includes the time
 # the command took as well as all output from the commands.
 ( time eval ${cmd} ) 2>&1 | tee ${log}
